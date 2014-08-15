168779eb3bb5d1cd0006ef9cec33d31b330f7d3d
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-15 14:58:28 -0400

    added home page

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import  render_to_response, RequestContext
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def home(request):
+    return render_to_response('angular.html', {}, RequestContext(request))
