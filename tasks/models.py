912419adafb7466688df1838fd79df94c424e724
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-15 16:37:27 -0400

    added deleted flag

--- tasks/models.py
+++ tasks/models.py
@@ -11,6 +11,7 @@ class Task(models.Model):
     account = models.ForeignKey(User)
     title = models.CharField(max_length=100, blank=False)
     completed = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
 
     date_added = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
