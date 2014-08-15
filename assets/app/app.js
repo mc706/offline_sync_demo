9bf1955292114ca1d71d20ca32e752fe08de8534
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-15 16:11:20 -0400

    updated angular

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,2 +1,24 @@
-var app = angular.module('tasks', []);
+var app = angular.module('tasks', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    'use strict';
+    console.log('routes setup');
+    $routeProvider
+        .when('/', {
+            controller: 'TaskController',
+            templateURL: '/static/app/views/home.html',
+            resolve: {
+                tasks: function (TaskService) {
+                    return TaskService.getTasks();
+                }
+            }
+        })
+        .when('/edit', {
+            controller: 'TaskController',
+            templateURL: 'editor.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
 
