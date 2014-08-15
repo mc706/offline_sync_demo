c6691a5d6dc477c50954885751123ddb10863e49
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-15 13:24:04 -0400

    added tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from uuidfield import UUIDField
 
-# Create your models here.
+
+class Task(models.Model):
+    """
+    Simple task model, has UUID field 'key'
+    """
+    key = UUIDField(auto=True)
+    account = models.ForeignKey(User)
+    title = models.CharField(max_length=100, blank=False)
+    completed = models.BooleanField(default=False)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'task'
+        verbose_name_plural = 'tasks'
