928aa398a5579fef26a67469bfa0e94f1b41ab9f
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-21 16:50:45 -0400

    updated database

--- offline_sync_demo/settings.py
+++ offline_sync_demo/settings.py
@@ -60,8 +60,10 @@ WSGI_APPLICATION = 'offline_sync_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'offline',
+        'USER': 'django',
+        'PASSWORD': '',
     }
 }
 
