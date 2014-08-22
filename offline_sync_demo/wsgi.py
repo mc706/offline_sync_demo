b682cd41d6793e57a9c2c37374da26275617a3fb
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-22 08:36:18 -0400

    reverted wsgi

--- offline_sync_demo/wsgi.py
+++ offline_sync_demo/wsgi.py
@@ -7,19 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
-import os, sys, site
-
-# Tell wsgi to add the Python site-packages to its path.
-site.addsitedir('~/.virtualenvs/offline/lib/python2.7/site-packages')
-
+import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "offline_sync_demo.settings")
-
-activate_this = os.path.expanduser("~/.virtualenvs/offline/bin/activate_this.py")
-exec(compile(open(activate_this, "rb").read(), activate_this, 'exec'))
-
-# Calculate the path based on the location of the WSGI script
-workspace = os.path.abspath(os.path.dirname(__name__))
-sys.path.append(workspace)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
