c2e961d50f3294c65f557e6f6a2502b563d248dd
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-22 09:07:44 -0400

    updated admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from tasks.models import Task
 
-
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['key', 'account', 'title', 'completed', 'deleted', 'date_modified']
     search_fields = ['title']
     list_filter = ['account', 'completed']
-
-
-admin.site.register(Task, TaskAdmin)
